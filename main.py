e4f00470b9dd6ba6e3dd5d2ecf6cfdbddcd00a17
zayatnik <55553752+zayatnik@users.noreply.github.com>
2022-04-04 00:29:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,33 @@ from tkinter.messagebox import *
 import numpy as np
 from numpy import linalg as LA
 import math
+import networkx as nx
+import matplotlib.pyplot as plt
+
 
 n = 1
 myfont = 'Courier 30'
+colors = ['0', 'g', 'b']
+arrowstyles = ['-', '<-']
+
+def firsts(path):
+    res = []
+    for i in path:
+        res.append(i[0])
+    return res
+
+def dfs(graph, start, end):
+    fringe = [(start, [])]
+    while fringe:
+        state, path = fringe.pop()
+        if path and state == end:
+            yield path
+            continue
+        for next_state in graph[state]:
+            if next_state[0] in firsts(path):
+                continue
+            fringe.append((next_state[0], path+[next_state]))
+
 
 def sortbyw(w, v):
     L = len(w)
@@ -16,6 +40,18 @@ def sortbyw(w, v):
                 for k in range(L):
                     v[k][j], v[k][j + 1] = v[k][j + 1], v[k][j]
     return w, v
+
+
+class Cycle:
+    def __init__(self, cycle):
+        self.cycle = cycle
+
+    def __str__(self):
+        res = str(self.cycle[0])
+        for i in self.cycle[1:]:
+            res += f' -->{(i[1])}--> {i[0]}'
+        return res
+
 
 def click_button():
     global n
@@ -77,6 +113,7 @@ def click_button():
     b2['bg'] = 'aquamarine'
     b2.grid(row = 2 * n + 2, column = 0, columnspan = 3 * n + 3)
 
+
 def click_button2(loe1, loe2, loe3, loe4, loe5, loe6, n):
     global l2, l3, l4, l2i, l3i, l4i
     l2.destroy()
@@ -102,7 +139,7 @@ def click_button2(loe1, loe2, loe3, loe4, loe5, loe6, n):
             lon[3][i].append(float(loe4[i][j].get())) #C2
             lon[4][i].append(float(loe5[i][j].get())) #B2
             lon[5][i].append(float(loe6[i][j].get())) #M2
-    er1 = er2 = er3 = False
+    er1 = er2 = er3 = er4 = False
     for i in range(n):
         for j in range(n):
             if lon[0][i][j] != lon[0][j][i] or lon[1][i][j] != lon[1][j][i] or lon[2][i][j] != lon[2][j][i]:
@@ -117,12 +154,17 @@ def click_button2(loe1, loe2, loe3, loe4, loe5, loe6, n):
             if lon[3][i][j] != 0 and lon[3][j][i] != 0 and i != j or lon[4][i][j] != 0 and lon[4][j][i] != 0 and i !=j or lon[5][i][j] != 0 and lon[5][j][i] != 0 and i !=j:
                 er3 = True
                 break
+            if lon[0][i][j] == 0 and i == j or lon[1][i][j] == 0 and i == j or lon[2][i][j] == 0 and i == j:
+                er4 = True
+                break
     if er1:
         showerror('Ошибка!', 'Матрицы взаимных связей должны быть симметричными!')
     elif er2:
         showerror('Ошибка!', 'Матрицы направленных связей должны иметь нулевые диагонали!')
     elif er3:
         showerror('Ошибка!', 'Если один элемент матрицы напрвленных связей равен нулю, то другой должен быть ненулевым, и наоборот!')
+    elif er4:
+        showerror('Ошибка!', 'Диагонали матриц взаимных связей не должны содержать нулей!')
     else:
         MC = np.dot(LA.inv(lon[2]), lon[0]) #M^(-1)*C
         w, v = LA.eig(MC)
@@ -139,7 +181,7 @@ def click_button2(loe1, loe2, loe3, loe4, loe5, loe6, n):
         l3.place(relx = 0.5, rely = 0.55, anchor = 'c')
         l3i = list()
         for i in range(n):
-            l3i.append(Label(window, text = str(round(w1[i] / (2 * math.pi), 5)), font = myfont))
+            l3i.append(Label(window, text = str(round(w1[i] / (2 * math.pi), 5)) + ' Гц', font = myfont))
         for i in range(n):
             l3i[i].place(relx = 1.0 / float(n + 1) * (i + 1), rely = 0.6, anchor='c')
         l4 = Label(window, text = 'Собственные векторы:', font = myfont)
@@ -147,12 +189,82 @@ def click_button2(loe1, loe2, loe3, loe4, loe5, loe6, n):
         l4i = list()
         for i in range(n):
             l4ij = list()
-            for j in range (n):
+            for j in range(n):
                 l4ij.append(Label(window, text = str(round(v[i][j], 5)), font = myfont))
             l4i.append(l4ij)
         for i in range(n):
             for j in range(n):
                 l4i[i][j].place(relx = 1.0 / float(n + 1) * (j + 1), rely = 0.75 + i * 0.05, anchor = 'c')
+
+
+        graph = []
+        for i in range(n):
+            graph.append([])
+        for i in range(3):
+            for j in range(n):
+                for k in range(j):
+                    if lon[i][j][k] != 0:
+                        graph[j].append((k, i))
+                        graph[k].append((j, i))
+        for i in range(3, 6):
+            for j in range(n):
+                for k in range(n):
+                    if lon[i][j][k] > 0:
+                        graph[j].append((k, i))
+        cycles = [[node] + path for node in range(n) for path in dfs(graph, node, node)]
+        good_cycles = sorted(cycles)
+        very_good_cycles = []
+        for i in good_cycles:
+            very_good_cycles.append(Cycle(i))
+
+        types = {}
+        for i, val in enumerate(graph):
+            for j in val:
+                if not (i, j[0]) in types:
+                    types[(i, j[0])] = []
+                types[(i, j[0])].append(j[1])
+        for i in types:
+            for j in types:
+                if (j[1], j[0]) == (i[0], i[1]):
+                    for k in types[i]:
+                        if k < 3 and k in types[j]:
+                            types[j].remove(k)
+
+        G = nx.DiGraph()
+        for i in types.keys():
+            G.add_edges_from([i])
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_color='r', node_size=5000, alpha=1)
+        nx.draw_networkx_labels(G, pos)
+        ax = plt.gca()
+        for e in G.edges:
+            for i in types[e]:
+                ax.annotate("",
+                            xy=pos[e[0]], xycoords='data',
+                            xytext=pos[e[1]], textcoords='data',
+                            arrowprops=dict(arrowstyle=arrowstyles[i // 3], color=colors[i % 3],
+                                            shrinkA=40, shrinkB=40,
+                                            patchA=None, patchB=None,
+                                            connectionstyle="arc3,rad=rrr".replace('rrr', str(0.1 * i), ), linewidth=4
+                                            ),
+                            )
+        xmin, xmax, ymin, ymax = 0, 0, 0, 0
+        for i in pos.values():
+            if i[0] < xmin:
+                xmin = i[0]
+            if i[0] > xmax:
+                xmax = i[0]
+            if i[1] < ymin:
+                ymin = i[1]
+            if i[1] > ymax:
+                ymax = i[1]
+        plt.xlim([xmin - 0.5, xmax + 0.5])
+        plt.ylim([ymin - 0.5, ymax + 0.5])
+        plt.axis('off')
+        plt.show()
+
+
+
 
 window = Tk()
 window['bg'] = 'aquamarine'
